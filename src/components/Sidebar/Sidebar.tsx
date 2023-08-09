058fdad69d531a07688839b627eff285c76733b0
Andreirl032 <andrei.ramos.lopes@gmail.com>
2023-08-08 22:37:01 -0300

    correção da sidebar não ocultando

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,10 +3,15 @@ import "./Sidebar.css";
 import { SidebarContext } from "@/app/context/SidebarContext";
 
 export const Sidebar = () => {
-  const { isOpen } = useContext(SidebarContext);
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
   return (
     <>
-      <div className={`overlay ${isOpen ? "" : "no-opacity"}`} />
+      <div
+        className={`overlay ${isOpen ? "" : "no-opacity"}`}
+        onClick={() => {
+          setIsOpen(false);
+        }}
+      />
       <aside className={`sidebar ${isOpen ? "" : "closed"}`}>
         <ul>
           <li className="sidebar-elements">Oi</li>
